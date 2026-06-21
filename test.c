3e8e9bd4e3068e062b9df2fe4783534d9baca9c0
infinitsi <infinitytamba@gmail.com>
2026-06-21 10:41:07 -0500

    guess number game

--- test.c
+++ test.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char *argv[]) {
-    // Example template code
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main() {
+    time_t currTime;
+    time(&currTime);
+    srand(currTime);
+    int num = rand() % 51;
+    int input;
+    int guesses = 0;
+    do {
+        printf("Enter a number 0-50: ");
+        scanf("%d", &input);
+        guesses++;
+        if(num > input) {
+            printf("Greater\n");
+        }
+        if(num < input) {
+            printf("Less\n");
+        }
+    } while(input != num);
+    printf("You guessed it! It was %d", num);
+    return 0;
 }
